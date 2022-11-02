733e2baa41393ba25dacdd049556cdf8b878d379
khaledamari48@gmail.com <khaledamari48@gmail.com>
2022-11-02 18:34:07 +0100

    Hello

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+// Variables
+
+// Supabase Installation Variables
 const SupabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzbHF4YXdhamhoemJwaG5jdmJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjcxMjY5OTAsImV4cCI6MTk4MjcwMjk5MH0.gn_2JOlY9aj4EED2MfD_9honCPzqJGxqkwbq8RBIOWI'
 const SupabaseUrl = 'https://cslqxawajhhzbphncvbp.supabase.co'
 const options = {
@@ -14,13 +17,27 @@ const options = {
     headers: { 'x-my-custom-header': 'ShortAllAroundNews' },
   },
 }
+// DOM Elements
+
+const NewsSection = document.querySelector('#NewsSection')
 
 const supabase = createClient(SupabaseUrl, SupabaseKey, options)
+// functions
+  
 
 async function Init() {
 let { data: News, error } = await supabase
   .from('News')
   .select('*')
-  console.log(News[1]);
+  console.log(News)
+  News.forEach((data) => {
+    // future me (use document.createElement) and the apeend it to body
+  NewsSection.innerHTML = `
+  <div id="NewsTeller">
+  <h1 id="HeadLine">${data.Title}</h1>
+ <p id="Info">${data.Info}</p>
+ </div>`
+ console.log(News);
+})
 }
 Init()
