8ac6563f64489b0384250f166318a07944625457
khaledamari48@gmail.com <khaledamari48@gmail.com>
2023-01-04 23:58:22 +0100

    revert

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+import{createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 import ApiKeys from './ApiKeys.js'
 const NewsApiKey=ApiKeys.NewsApiKey
 const SupabaseKey=ApiKeys.SupabaseKey
 const SupabaseUrl=ApiKeys.SupabaseUrl
 const options={db:{schema:'public',},auth:{autoRefreshToken:!0,persistSession:!0,detectSessionInUrl:!0},global:{headers:{'x-my-custom-header':'ShortAllAroundNews'},},}
 const NewsSection=document.querySelector('#NewsSection')
-const Supabase=supabase.createClient(SupabaseUrl,SupabaseKey,options)
+const Supabase=createClient(SupabaseUrl,SupabaseKey,options)
 async function AddDataToDataBase(){}
 async function Init(){let{data:News,error}=await Supabase.from('News').select('*').order('CreatedAt', {ascending: false})
 News.forEach((data)=>{let NewsTeller=document.createElement('article')
