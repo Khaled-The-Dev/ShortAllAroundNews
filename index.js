e82a19533a126be456afd7050beaad259157f65f
khaledamari48@gmail.com <khaledamari48@gmail.com>
2023-01-05 00:52:16 +0100

    revert

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-import{createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 import ApiKeys from './ApiKeys.js'
+import{createClient}from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 const NewsApiKey=ApiKeys.NewsApiKey
 const SupabaseKey=ApiKeys.SupabaseKey
 const SupabaseUrl=ApiKeys.SupabaseUrl
 const options={db:{schema:'public',},auth:{autoRefreshToken:!0,persistSession:!0,detectSessionInUrl:!0},global:{headers:{'x-my-custom-header':'ShortAllAroundNews'},},}
 const NewsSection=document.querySelector('#NewsSection')
-const Supabase=createClient(SupabaseUrl,SupabaseKey,options)
+const supabase=createClient(SupabaseUrl,SupabaseKey,options)
 async function AddDataToDataBase(){}
-async function Init(){let{data:News,error}=await Supabase.from('News').select('*').order('CreatedAt', {ascending: false})
+async function Init(){let{data:News,error}=await supabase.from('News').select('*').order('CreatedAt', {ascending: false})
 News.forEach((data)=>{let NewsTeller=document.createElement('article')
 NewsTeller.id='NewsTeller'
-NewsTeller.innerHTML=`<img src="${data.ImageUrl}" width=100% height=600 loading="lazy" alt="${data.ImgAlt}/><h1>${data.Title}</h1><p>${data.Info}</p>`
+NewsTeller.className = 'column'
+NewsTeller.innerHTML=`
+   <img src="${data.ImageUrl}" width=100% height=400 loading="lazy" alt="${data.ImgAlt}"></img>
+  <h1 id="HeadLine">${data.Title}</h1>
+ <p id="Info">${data.Info}</p>
+ `
 NewsSection.append(NewsTeller)})}
 Init()
