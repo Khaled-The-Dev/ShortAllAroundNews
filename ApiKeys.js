cfd9a610ec786d224f879469a179b535e755712a
khaledamari48@gmail.com <khaledamari48@gmail.com>
2023-02-07 19:24:31 +0100

    OpenSource

--- ApiKeys.js
+++
@@ -1,5 +0,0 @@
-const ApiKeys={
-SupabaseUrl: 'https://cslqxawajhhzbphncvbp.supabase.co',
-SupabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzbHF4YXdhamhoemJwaG5jdmJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjcxMjY5OTAsImV4cCI6MTk4MjcwMjk5MH0.gn_2JOlY9aj4EED2MfD_9honCPzqJGxqkwbq8RBIOWI',
-}
-export default ApiKeys
